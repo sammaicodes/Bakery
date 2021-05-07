3bb9398c8bf3c951e24ff354763c6a6df730f24e
Sammai Gutierrez <sammaigtz@hotmail.com>
2021-05-07 09:47:42 -0700

    Fail 1st test and files working as expected.

---
+++ PierresBakery.Tests/ModelTests/BreadTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery.Models;
+using System;
+
+namespace Bakery.Test
+{
+  [TestClass]
+  public class BreadTests
+  {
+    [TestMethod]
+    public void CalculateBreadCost_ReceiveUserInput_Number()
+    {
+      int userQuantity = Bread.CalculateBreadCost(3);
+      int numberTest = 2;
+      Assert.AreEqual(userQuantity, numberTest);
+    }
+  }
+}
