e92441530c656e21deba0773c86436d08c105e9e
Sammai Gutierrez <sammaigtz@hotmail.com>
2021-05-07 15:27:20 -0700

    Begin to write the console app on Program.cs.

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -1,13 +1,18 @@
-// using System;
-// using Bakery.Models;
+using System;
+using Bakery.Models;
 
-// namespace Bakery
-// {
-//   public class Program
-//   {
-//     public static void Main()
-//     {
+namespace Bakery
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Welcome to Pierre's Bakery!");
+      Console.WriteLine("Deals of the day:");
+      Console.WriteLine("~ On bread: Buy 2, get 1 free, and a single loaf costs $5.");
+      Console.WriteLine("~ On pastries: Buy 3 for $5 or buy 1 for $2");
+      
 
-//     }
-//   }
-// }
+    }
+  }
+}
